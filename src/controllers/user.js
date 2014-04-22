3b9422b48f3c9be242ec2b14c6212bc3b5b585aa
stormslowly <stormslowly@gmail.com>
2014-04-22 09:08:33 +0800

    remove jshint error

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,20 +1,14 @@
 'use strict';
 /* @see ./routes.coffee for routing*/
 
-var url = require('url');
-var ipn = require('paypal-ipn');
 var _ = require('lodash');
 var nconf = require('nconf');
 var async = require('async');
 var shared = require('habitrpg-shared');
-var validator = require('validator');
-var check = validator.check;
-var sanitize = validator.sanitize;
 var User = require('./../models/user').model;
 var ga = require('./../utils').ga;
 var Group = require('./../models/group').model;
 var Challenge = require('./../models/challenge').model;
-var logging = require('./../logging');
 var acceptablePUTPaths;
 var api = module.exports;
 var isProduction = nconf.get("NODE_ENV") === "production";
@@ -25,12 +19,13 @@ var paypalRecurring = new PaypalRecurring({
   password:  nconf.get('PAYPAL_PASSWORD'),
   signature: nconf.get('PAYPAL_SIGNATURE')
 }, isProduction ? "production" : "sandbox");
+
 var paypalCheckout = require('paypal-express-checkout')
   .init(nconf.get('PAYPAL_USERNAME'), nconf.get('PAYPAL_PASSWORD'), nconf.get('PAYPAL_SIGNATURE'), nconf.get('BASE_URL'), nconf.get('BASE_URL'), !isProduction);
 
 // api.purchase // Shared.ops
 
-api.getContent = function(req, res, next) {
+api.getContent = function(req, res) {
   res.json(shared.content);
 };
 
@@ -46,7 +41,7 @@ api.getContent = function(req, res, next) {
   ---------------
 */
 
-api.findTask = function(req, res, next) {
+api.findTask = function(req, res ) {
   var task = res.locals.user.tasks[req.params.id];
   return task;
 };
@@ -72,7 +67,7 @@ api.score = function(req, res, next) {
     return res.json(400, {err: ':id required'});
   }
   if (direction !== 'up' && direction !== 'down') {
-    if (direction == 'unlink' || direction == 'sort') return next();
+    if (direction === 'unlink' || direction ==='sort') {return next();}
     return res.json(400, {err: ":direction must be 'up' or 'down'"});
   }
   // If exists already, score it
@@ -131,7 +126,7 @@ api.score = function(req, res, next) {
           return chal.syncToUser(user);
         }
 
-        if (!t) return chal.syncToUser(user); // this task was removed from the challenge, notify user
+        if (!t) {return chal.syncToUser(user);} // this task was removed from the challenge, notify user
         t.value += delta;
         if (t.type === 'habit' || t.type === 'daily'){
           t.history.push({value: t.value, date: +new Date()});
@@ -144,7 +139,7 @@ api.score = function(req, res, next) {
 /**
  * Get all tasks
  */
-api.getTasks = function(req, res, next) {
+api.getTasks = function(req, res ) {
   var user = res.locals.user;
   if (req.query.type) {
     return res.json(user[req.query.type+'s']);
@@ -156,7 +151,7 @@ api.getTasks = function(req, res, next) {
 /**
  * Get Task
  */
-api.getTask = function(req, res, next) {
+api.getTask = function(req, res ) {
   var task = findTask(req,res);
   if (!task){
     return res.json(404, {err: "No task found."});
@@ -190,7 +185,7 @@ api.getTask = function(req, res, next) {
 /**
  * Get User
  */
-api.getUser = function(req, res, next) {
+api.getUser = function(req, res) {
   var user = res.locals.user.toJSON();
   user.stats.toNextLevel = shared.tnl(user.stats.lvl);
   user.stats.maxHealth = 50;
@@ -211,11 +206,11 @@ api.getUser = function(req, res, next) {
  */
 acceptablePUTPaths = _.reduce(require('./../models/user').schema.paths, function(m,v,leaf){
   var found= _.find('achievements filters flags invitations lastCron party preferences profile stats'.split(' '), function(root){
-    return leaf.indexOf(root) == 0;
+    return leaf.indexOf(root) === 0;
   });
-  if (found) m[leaf]=true;
+  if (found) {m[leaf]=true;}
   return m;
-}, {})
+}, {});
 
 //// Uncomment this if we we want to disable GP-restoring (eg, holiday events)
 //_.each('stats.gp'.split(' '), function(removePath){
@@ -231,18 +226,20 @@ acceptablePUTPaths = _.reduce(require('./../models/user').schema.paths, function
 api.update = function(req, res, next) {
   var user = res.locals.user;
   var errors = [];
-  if (_.isEmpty(req.body)) return res.json(200, user);
+  if (_.isEmpty(req.body)) {return res.json(200, user);}
 
   _.each(req.body, function(v, k) {
-    if (acceptablePUTPaths[k])
+    if (acceptablePUTPaths[k]){
       user.fns.dotSet(k, v);
-    else
+    }
+    else{
       errors.push("path `" + k + "` was not saved, as it's a protected path. See https://github.com/HabitRPG/habitrpg/blob/develop/API.md for PUT /api/v2/user.");
+    }
     return true;
   });
   user.save(function(err) {
-    if (!_.isEmpty(errors)) return res.json(401, {err: errors});
-    if (err) return next(err);
+    if (!_.isEmpty(errors)) { return res.json(401, {err: errors}); }
+    if (err) {return next(err);}
     res.json(200, user);
   });
 };
@@ -253,9 +250,9 @@ api.cron = function(req, res, next) {
     ranCron = user.isModified(),
     quest = shared.content.quests[user.party.quest.key];
 
-  if (ranCron) res.locals.wasModified = true;
-  if (!ranCron) return next(null,user);
-  if (!quest) return user.save(next);
+  if (ranCron) { res.locals.wasModified = true; }
+  if (!ranCron) { return next(null,user);}
+  if (!quest) {return user.save(next);}
 
   // If user is on a quest, roll for boss & player, or handle collections
   // FIXME this saves user, runs db updates, loads user. Is there a better way to handle this?
@@ -283,13 +280,14 @@ api.cron = function(req, res, next) {
 // api.reset // Shared.ops
 
 api['delete'] = function(req, res, next) {
-  if (res.locals.user.purchased.plan.customerId)
+  if (res.locals.user.purchased.plan.customerId){
     return res.json(400,{err:"You have an active subscription, cancel your plan before deleting your account."});
+  }
   res.locals.user.remove(function(err){
-    if (err) return next(err);
+    if (err) {return next(err);}
     res.send(200);
-  })
-}
+  });
+};
 
 /*
  ------------------------------------------------------------------------
@@ -303,10 +301,10 @@ api.addTenGems = function(req, res, next) {
   var user = res.locals.user;
   user.balance += 2.5;
   user.save(function(err){
-    if (err) return next(err);
+    if (err) {return next(err);}
     res.send(204);
-  })
-}
+  });
+};
 
 /*
  ------------------------------------------------------------------------
@@ -326,20 +324,24 @@ api.cast = function(req, res, next) {
   var user = res.locals.user;
   var targetType = req.query.targetType;
   var targetId = req.query.targetId;
-  var klass = shared.content.spells.special[req.params.spell] ? 'special' : user.stats.class
+  var klass = shared.content.spells.special[req.params.spell] ? 'special' : user.stats.class;
   var spell = shared.content.spells[klass][req.params.spell];
-  if (!spell) return res.json(404, {err: 'Spell "' + req.params.spell + '" not found.'});
+  if (!spell){
+    return res.json(404, {err: 'Spell "' + req.params.spell + '" not found.'});
+  }
 
   var done = function(){
     var err = arguments[0];
-    var saved = _.size(arguments == 3) ? arguments[2] : arguments[1];
-    if (err) return next(err);
+    var saved = _.size(arguments === 3) ? arguments[2] : arguments[1];
+    if (err) { return next(err);}
     res.json(saved);
-  }
+  };
 
   switch (targetType) {
     case 'task':
-      if (!user.tasks[targetId]) return res.json(404, {err: 'Task "' + targetId + '" not found.'});
+      if (!user.tasks[targetId]){
+        return res.json(404, {err: 'Task "' + targetId + '" not found.'});
+      }
       spell.cast(user, user.tasks[targetId]);
       user.save(done);
       break;
@@ -359,23 +361,23 @@ api.cast = function(req, res, next) {
           // Solo player? let's just create a faux group for simpler code
           var g = group ? group : {members:[user]};
           var series = [], found;
-          if (targetType == 'party') {
+          if (targetType === 'party') {
             spell.cast(user, g.members);
-            series = _.transform(g.members, function(m,v,k){
-              m.push(function(cb2){v.save(cb2)});
+            series = _.transform( g.members, function(m,v){
+              m.push(function(cb2){v.save(cb2);});
             });
           } else {
-            found = _.find(g.members, {_id: targetId})
+            found = _.find(g.members, {_id: targetId});
             spell.cast(user, found);
-            series.push(function(cb2){found.save(cb2)});
+            series.push(function(cb2){found.save(cb2);});
           }
 
           if (group) {
             series.push(function(cb2){
-              var message = '`'+user.profile.name+' casts '+spell.text + (targetType=='user' ? ' on '+found.profile.name : ' for the party')+'.`';
+              var message = '`'+user.profile.name+' casts '+spell.text + (targetType==='user' ? ' on '+found.profile.name : ' for the party')+'.`';
               group.sendChat(message);
               group.save(cb2);
-            })
+            });
           }
 
           async.series(series, cb);
@@ -386,7 +388,7 @@ api.cast = function(req, res, next) {
       ], done);
       break;
   }
-}
+};
 
 /**
  * All other user.ops which can easily be mapped to habitrpg-shared/index.coffee, not requiring custom API-wrapping
@@ -397,18 +399,20 @@ _.each(shared.wrap({}).ops, function(op,k){
       res.locals.user.ops[k](req,function(err, response){
         // If we want to send something other than 500, pass err as {code: 200, message: "Not enough GP"}
         if (err) {
-          if (!err.code) return next(err);
-          if (err.code >= 400) return res.json(err.code,{err:err.message});
+          if (!err.code) {return next(err);}
+          if (err.code >= 400){
+            return res.json(err.code,{err:err.message});
+          }
           // In the case of 200s, they're friendly alert messages like "You're pet has hatched!" - still send the op
         }
         res.locals.user.save(function(err){
-          if (err) return next(err);
+          if (err) {return next(err);}
           res.json(200,response);
-        })
+        });
       }, ga);
-    }
+    };
   }
-})
+});
 
 /*
   ------------------------------------------------------------------------
@@ -427,18 +431,19 @@ api.batchUpdate = function(req, res, next) {
 
   // Stash user.save, we'll queue the save op till the end (so we don't overload the server)
   var oldSave = user.save;
-  user.save = function(cb){cb(null,user)}
+  user.save = function(cb){cb(null,user);};
 
   // Setup the array of functions we're going to call in parallel with async
   res.locals.ops = [];
   var ops = _.transform(req.body, function(m,_req){
-    if (_.isEmpty(_req)) return;
+    if (_.isEmpty(_req)) {return;}
     m.push(function() {
       var cb = arguments[arguments.length-1];
       res.locals.ops.push(_req);
       res.send = res.json = function(code, data) {
-        if (_.isNumber(code) && code >= 500)
+        if (_.isNumber(code) && code >= 500){
           return cb(code+": "+ (data.message ? data.message : data.err ? data.err : JSON.stringify(data)));
+        }
         return cb();
       };
       api[_req.op](_req, res, cb);
@@ -454,7 +459,7 @@ api.batchUpdate = function(req, res, next) {
   async.waterfall(ops, function(err,user) {
     res.json = oldJson;
     res.send = oldSend;
-    if (err) return next(err);
+    if (err) {return next(err);}
 
     var response = user.toJSON();
     response.wasModified = res.locals.wasModified;
